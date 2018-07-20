e0b49afcb666f579b416e638142ef9b0b4e5148b
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-19 18:12:59 -0700

    adds menu button using jquery

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
+function Menu() {
+  this.items = ['Home', 'About', 'Main', 'Contact', 'Blog']
+}
 
+Menu.prototype.renderMenuButton = function() {
+  $('body').append(
+    '<nav id="nav"><button>Menu</button></nav>'
+    )
+}
+
+Menu.prototype.initialize = function() {
+  this.renderMenuButton()
+}
+
+new Menu().initialize()
